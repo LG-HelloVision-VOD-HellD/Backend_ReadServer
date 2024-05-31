d880183b8a5a50d14e8a1f3b87ac37375dd9df0f
semin-pk <tpals11149@gmail.com>
2024-05-31 12:35:12 +0900

    sort episode, season

--- app/routes/detailpage/routes/kids/season/episode/fuc.py
+++ app/routes/detailpage/routes/kids/season/episode/fuc.py
@@ -5,7 +5,7 @@ db = client['hellody']
 
 async def episode_detail(season_id):
     collection = db['KIDS_EPISODE']
-    cursor = collection.find({'K_SEASON_ID': season_id}, {'_id': 0})
+    cursor = collection.find({'K_SEASON_ID': season_id}, {'_id': 0}).sort({'K_EPISODE_NUM':1})
     vod_list = await cursor.to_list(length=100)
     print(vod_list)
     return vod_list
--- app/routes/detailpage/routes/series/season/episode/fuc.py
+++ app/routes/detailpage/routes/series/season/episode/fuc.py
@@ -5,7 +5,7 @@ db = client['hellody']
 
 async def episode_detail(season_id):
     collection = db['EPISODE']
-    cursor = collection.find({'SEASON_ID': season_id}, {'_id': 0})
+    cursor = collection.find({'SEASON_ID': season_id}, {'_id': 0}).sort({'EPISODE_NUM':1})
     vod_list = await cursor.to_list(length=100)
     print(vod_list)
     return vod_list
