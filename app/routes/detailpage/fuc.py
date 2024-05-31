8f19e6fbbf09547dfafd6f28ea8fd7005d2b2634
semin-pk <125566649+semin-pk@users.noreply.github.com>
2024-06-01 00:42:42 +0900

    add actor

--- app/routes/detailpage/fuc.py
+++ app/routes/detailpage/fuc.py
@@ -12,6 +12,11 @@ async def vod_detail(vod_id):
         print(vod_type)
         collection = db[vod_type]
         detail_vod = await collection.find_one({'VOD_ID':vod_id}, {'_id': 0})
+        if vod_type == 'MOVIES':
+            actor_collection = db['ACTOR']
+            cursor = actor_collection.find({'MOVIE_ID':detail_vod['MOVIE_ID']}, {'_id':0, 'ACTOR_NAME':1,'PROFILE':1})
+            actor = await cursor.to_list(length=100)
+            detail_vod['ACTOR'] = actor
         print(detail_vod)
         return detail_vod
     except:
