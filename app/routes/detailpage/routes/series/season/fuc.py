84999071fc058767ff46f01dfc1b821453978cad
semin-pk <tpals11149@gmail.com>
2024-06-17 19:19:34 +0900

    add episode_count

--- app/routes/detailpage/routes/series/season/fuc.py
+++ app/routes/detailpage/routes/series/season/fuc.py
@@ -7,6 +7,9 @@ async def season_detail(series_id):
     collection = db['SEASON']
     cursor = collection.find({'SERIES_ID': series_id}, {'_id': 0, 'SEASON_ID': 1, 'SEASON_NUM':1}).sort({'SEASON_NUM':1})
     vod_list = await cursor.to_list(length=100)
+    for vod in vod_list:
+        episode_count = await collection.find_one({'SEASON_ID':vod['SEASON_ID']}, {'_id':0, 'EPISODE_COUNT':1})
+        vod['EPISODE_COUNT'] = episode_count['EPISODE_COUNT']
     print(vod_list)
     return vod_list
     
