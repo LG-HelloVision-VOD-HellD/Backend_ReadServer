e303cdda0e3f551bad0fc7dcb05f92b64fb4e24a
semin-pk <125566649+semin-pk@users.noreply.github.com>
2024-06-19 14:43:42 +0900

    Update fuc.py

--- app/routes/mainpage/routes/home/fuc.py
+++ app/routes/mainpage/routes/home/fuc.py
@@ -21,7 +21,7 @@ async def vodlist_spotify(user_id: int):
 
 async def vodlist_youtube(user_id: int):
     collection = db['youtube_recommend']
-    cursor = collection.find({'user_id': user_id}, {'_id': 0, 'youtube_recommend': 1})
+    cursor = collection.find({}, {'_id': 0, 'youtube_recommend': 1})
     vod_list = await cursor.to_list(length=100)
     print(vod_list)
     return vod_list
